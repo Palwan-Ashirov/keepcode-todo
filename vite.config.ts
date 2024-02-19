c12864514b920941f988a29e85c7616866747fc5
Palwan Ashirov <palwan.ashirov@gmail.com>
2024-02-20 02:24:44 +0500

    init: reconfiguration typescript and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3001,
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@use': path.resolve(__dirname, './src/composables')
+    }
+  }
 })
